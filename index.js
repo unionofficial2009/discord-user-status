94db1130f0c10b3cca5addf7d5e6e90f14e61d1f
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-20 19:38:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     let status = newMember.user.presence.status;
     // get a reference to all channels in the user's guild
     let guildChannels = newMember.guild.channels;
-    let guildroles = newMember.guild.GuildMember.id;
+    let guildroles = newMember.guild.member.id;
     
     
   let bicon = newMember.user.displayAvatarURL;
