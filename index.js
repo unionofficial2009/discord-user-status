a41d0747fd07c140ce6b57f09e1a60984c0d9e0c
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-20 19:06:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     let status = newMember.user.presence.status;
     // get a reference to all channels in the user's guild
     let guildChannels = newMember.guild.channels;
-    //let guildroles = newMember.guild.roles;
+    let guildroles = newMember.guild.roles;
     
     
   let bicon = newMember.user.displayAvatarURL;
@@ -70,7 +70,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     .addField("Username", `${newMember.user.username}`)
     .addField("Tag", `${newMember.user.tag}`)
     .addField("ID", `${newMember.user.id}`)
-    .addField("Games", `${akmemberRole.name}`)
+    .addField("Games", `${guildroles}`)
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Status", `${newMember.user.presence.status}`)
