8e7a8b32fbb52d2bde47defc85b78afcab43c00b
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-20 18:16:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,38 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     let status = newMember.user.presence.status;
     // get a reference to all channels in the user's guild
     let guildChannels = newMember.guild.channels;
+    let guildroles = newMember.guild.roles;
     
     
   let bicon = newMember.user.displayAvatarURL;
   let bicon2 = bot.user.displayAvatarURL;  
   
+  let usergames ="";
+        
+  let akusergames = usergames.find(`name`, "AK - Member");
+  let dnusergames = usergames.find(`name`, "DN - Member");
+  let gcusergames = usergames.find(`name`, "GC - Member");
+  let rousergames = usergames.find(`name`, "RO - Member");
+  
+  if(akusergames) return usergames = usergames + "Aura Kingdom, ";
+  if(dnusergames) return usergames = usergames + "Dragon Nest, ";
+  if(gcusergames) return usergames = usergames + "Grand Chase, ";
+  if(rousergames) return usergames = usergames + "Ragnarok, ";
+  
+  if(usergames=="") return usergames = usergames + "N/A, ";  
+  
     
     
     if(oldMember.presence.status == newMember.presence.status && newMember.presence.status == "offline"){
         
-       
+        
         
         let botembed = new Discord.RichEmbed()
         .setDescription(`<@${newMember.user.id}>`)
         .addField("Username", `${newMember.user.username}`)
         .addField("Tag", `${newMember.user.tag}`)
         .addField("ID", `${newMember.user.id}`)
+        .addField("Games", `${usergames}`)
         .setColor("#15f153")
         .setThumbnail(bicon)
         .addField("Status", `${newMember.user.presence.status} (invisible)`)
@@ -45,6 +61,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     .addField("Username", `${newMember.user.username}`)
     .addField("Tag", `${newMember.user.tag}`)
     .addField("ID", `${newMember.user.id}`)
+    .addField("Games", `${usergames}`)
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Status", `${newMember.user.presence.status}`)
