f0033c5f1993bbb14344f77574c3ce4694ea552a
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-22 16:30:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     .addField("Username", `${newMember.user.username}`)
     .addField("Tag", `${newMember.user.tag}`)
     .addField("ID", `${newMember.user.id}`)
-    .addField("Roles", `${userroles.map(a => a.name)} - ${Object.keys(userroles).length`)
+    .addField("Roles", `${userroles.map(a => a.name)} - ${userroles.count`)
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Status", `${newMember.user.presence.status}`)
