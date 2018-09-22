6709485eacece67f4ad7bc46692b429d7fa2f6df
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-23 00:18:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
     
     if(userrolessize = 0){
       
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Hello maam/sir welcome to UNION.")
-    .addField("Instructions", "Please proceed to **#welcome** channel in the **UNION HEADQUATERS** discord server. Kindly read and follow the rules/instructions in the **#welcome** channel to access the channels for specific game.")
-    .addField("Need Help?", "Any problem, please DM **@UNION President  @UNION Vice President @UNION Officers @UNION Human Resources**.")
-    .setColor("#15f153")
-    .setTimestamp()
-    .setFooter("UNION User Notifications",bicon2);
+    //let botembed = new Discord.RichEmbed()
+    //.setDescription("Hello maam/sir welcome to UNION.")
+    //.addField("Instructions", "Please proceed to **#welcome** channel in the **UNION HEADQUATERS** discord server. Kindly read and follow the rules/instructions in the **#welcome** channel to access the channels for specific game.")
+    //.addField("Need Help?", "Any problem, please DM **@UNION President  @UNION Vice President @UNION Officers @UNION Human Resources**.")
+    //.setColor("#15f153")
+    //.setTimestamp()
+    //.setFooter("UNION User Notifications",bicon2);
    
-    newMember.send(botembed);    
+    //newMember.send(botembed);    
         
        
     } 
